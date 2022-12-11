73863088a57995d5c71427830092629a7fba1943
sidgrunion <aylenp17@hotmail.com>
2022-12-10 22:07:43 -0500

    Exercise 02_repeatString was resolved with no errors

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
-
+    let frase= "";
+    
+    if (num >= 0){
+    for (let i = 0; i<num;i++){
+       frase = frase + string
+    }
+    return frase;
+    }
+    else{
+        return "ERROR";
+    }
+}
+   
 // Do not edit below this line
 module.exports = repeatString;
